67d3709d7a776e84246b5b3fa1ca28187116f07d
Seva Safris <seva@safris.org>
2019-11-18 13:42:00 +0800

    Update javadocs

--- xjc/src/main/java/org/openjax/jaxb/xjc/XJCompiler.java
+++ xjc/src/main/java/org/openjax/jaxb/xjc/XJCompiler.java
@@ -21,7 +21,6 @@ import java.io.FileNotFoundException;
 import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
